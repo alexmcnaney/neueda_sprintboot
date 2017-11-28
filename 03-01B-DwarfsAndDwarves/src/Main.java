ea7d677ab3e44ed6d04aa41e146829bf970e668a
alex <alexmcnaney@gmail.com>
2017-11-28 13:29:04 +0000

    Dwarfs

---
+++ 03-01B-DwarfsAndDwarves/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+	
+	
+		Dwarf sleepy = new Dwarf();
+		sleepy.setName("Sleepy");
+		sleepy.setAuthor("Disney");
+		sleepy.setImage("sleepy.png");
+		
+		Dwarf happy = new Dwarf();
+		sleepy.setName("Happy");
+		sleepy.setAuthor("Disney");
+		sleepy.setImage("happy.png");
+		
+		Dwarf grumpy = new Dwarf();
+		sleepy.setName("Grumpy");
+		sleepy.setAuthor("Disney");
+		sleepy.setImage("grumpy.png");
+	}
+
+}
---
+++ O3-O1A-Java-DwarfsAndDwarves/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> dwarfs = new ArrayList<String>();
+		dwarfs.add("Happy");
+		dwarfs.add("Sleepy");
+		dwarfs.add("Dopey");
+		dwarfs.add("Bashful");
+		dwarfs.add("Sneezy");
+		dwarfs.add("Doc");
+		dwarfs.add("Grumpy");
+		
+	
+	
+	ArrayList<String> dwarves = new ArrayList<String>();
+	dwarves.add("Thorin");
+	dwarves.add("Balin");
+	dwarves.add("Dwalin");
+	dwarves.add("Kili");
+	dwarves.add("Nori");
+	dwarves.add("Ori");
+	dwarves.add("Oin");
+	dwarves.add("Gloin");
+	dwarves.add("Bifur");
+	dwarves.add("Bofur");
+	dwarves.add("Bombur");
+	//System.out.println("The seven dwarfs are " + dwarfs);
+	//System.out.println("The Company of Thorin consists of " + dwarves);
+	
+	System.out.println("The seven dwarfs are: ");
+	for (String dwarf: dwarfs ) {
+		System.out.println( dwarf);
+		
+		
+	}
+	
+	System.out.println("The Company of Thorin consists of: "  );
+	for (String dwarf: dwarves ) {
+		System.out.println(dwarf);
+		
+		
+	}
+	
+	
+	
+	
+}
+	
+	
+	
+}
