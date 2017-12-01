1849519ff6d8660b38bfd3d1be3860c601f6062c
alex <alexmcnaney@gmail.com>
2017-12-01 09:45:46 +0000

    Largest Buildings project completed. Added DwarfSaved project.

--- 04-01A-SpringBoot-LargestBuildings/src/main/java/uk/ac/belfastmet/buildings/controller/FloorAreaController.java
+++ 04-01A-SpringBoot-LargestBuildings/src/main/java/uk/ac/belfastmet/buildings/controller/FloorAreaController.java
@@ -12,21 +12,15 @@ import uk.ac.belfastmet.buildings.service.FloorAreaService;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/floorarea")
 public class FloorAreaController {
 	
 		//@Autowired
 		private FloorAreaService floorAreaService;
-		
-	 
-		
-		@GetMapping("/")
-		public String homePage(Model model) {
-			
-			return "homePage";
-		}
+		 
+	
 
-		@GetMapping("/floorarea")
+		@GetMapping("/")
 		public String floorarea(Model model) {
 			model.addAttribute("pageTitle", "floor area!");
 			this.floorAreaService = new FloorAreaService();
--- 04-01A-SpringBoot-LargestBuildings/src/main/java/uk/ac/belfastmet/buildings/service/FloorAreaService.java
+++ 04-01A-SpringBoot-LargestBuildings/src/main/java/uk/ac/belfastmet/buildings/service/FloorAreaService.java
@@ -26,15 +26,15 @@ public class FloorAreaService {
 		buildings.add(cw);
 		FloorArea afa = new FloorArea("5", "Aalsmeer Flower Auction","Netherlands","Aalsmeer","10,700,000 sqft", "afa.jpeg");
 		buildings.add(afa);		
-		FloorArea bcia = new FloorArea("6", "Beijing Capital International Airport Terminal 3","China","Beijing","10,610,000 sqft", "tvm.jpg");
+		FloorArea bcia = new FloorArea("6", "Beijing Capital International Airport Terminal 3","China","Beijing","10,610,000 sqft", "bcia.jpg");
 		buildings.add(bcia);		
-		FloorArea tvm = new FloorArea("7", "The Venetian Macao","Macau","Macau","10,500,000 sqft", "lol.jpg");
+		FloorArea tvm = new FloorArea("7", "The Venetian Macao","Macau","Macau","10,500,000 sqft", "tvm.jpg");
 		buildings.add(tvm);		
-		FloorArea scc = new FloorArea("8", "Sands Cotai Centrali","Macau","Macau","9,600,000 sqft", "lol.jpg");
+		FloorArea scc = new FloorArea("8", "Sands Cotai Centrali","Macau","Macau","9,600,000 sqft", "scc.jpg");
 		buildings.add(scc);
-		FloorArea cws = new FloorArea("9", "Ciputra World Surabaya","Indonesia","Surabaya","8,100,000 sqft", "lol.jpg");
+		FloorArea cws = new FloorArea("9", "Ciputra World Surabaya","Indonesia","Surabaya","8,100,000 sqft", "cws.jpg");
 		buildings.add(cws);		
-		FloorArea bts = new FloorArea("10", "Berjaya Times Square","Malaysia","Kuala Lumpur","7,500,000 sqft", "lol.jpg");
+		FloorArea bts = new FloorArea("10", "Berjaya Times Square","Malaysia","Kuala Lumpur","7,500,000 sqft", "bts.jpg");
 		buildings.add(bts);
 		
 	
