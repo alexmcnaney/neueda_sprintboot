7738c6f1c1a55ba27d49fd3748bb869042678d74
alex <alexmcnaney@gmail.com>
2017-12-12 14:36:44 +0000

    Add new cereal form added

--- 07-02-Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/CerealController.java
+++ 07-02-Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/CerealController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import uk.ac.belfastmet.cereal.domain.Cereal;
@@ -50,9 +51,18 @@ public class CerealController {
 	
 	@GetMapping("/cereal/add")
 	public String createCereal(Model model) {
-		model.addAttribute("pageTitle", "{cerealName}");
+		model.addAttribute("pageTitle", "Add");
 		model.addAttribute("cereal", new Cereal());
 		return "editCerealPage";
+	}
+	
+	@PostMapping("/cereal/save")
+	public String saveCereal(Cereal cereal) {
+	
+		Cereal savedCereal = cerealRepository.save(cereal);
+		savedCereal.getCerealId();
+		//redirecting user to the page with the new cereal id which was assigned by the database
+		return "redirect:/cereal/view/" + savedCereal.getCerealId();
 	}
 
 			
