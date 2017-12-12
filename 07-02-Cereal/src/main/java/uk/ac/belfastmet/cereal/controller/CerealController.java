ef1758aa8e57c9b834864b2cae24f22c23bf9ae0
alex <alexmcnaney@gmail.com>
2017-12-12 16:13:26 +0000

    Formatting

--- 07-02-Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/CerealController.java
+++ 07-02-Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/CerealController.java
@@ -46,73 +46,54 @@ public class CerealController {
 	@GetMapping("/cereal/view/{cerealId}")
 	public String viewCereal(@PathVariable("cerealId") Integer cerealId, Model model) {
 
-			model.addAttribute("pageTitle", "{cerealName}");
+		model.addAttribute("pageTitle", "{cerealName}");
 		model.addAttribute("cereal", cerealRepository.findOne(cerealId));
 		return "viewCerealPage";
 	}
-	
+
 	@GetMapping("/cereal/edit/{cerealId}")
 	public String editCereal(@PathVariable("cerealId") Integer cerealId, Model model) {
-		
+
 		model.addAttribute("pageTitle", "Edit cereal");
 		model.addAttribute("cereal", cerealRepository.findOne(cerealId));
 		return "editCerealPage";
-	
-		
+
 	}
-	
+
 	@GetMapping("/cereal/delete/{cerealId}")
 	public String deleteCereal(@PathVariable("cerealId") Integer cerealId) {
-		
+
 		cerealRepository.delete(cerealId);
 		return "redirect:/";
-	
-		
+
 	}
-	
-	
-	
-	
-	
-	
+
 	@GetMapping("/cereal/add")
 	public String createCereal(Model model) {
 		model.addAttribute("pageTitle", "Add");
 		model.addAttribute("cereal", new Cereal());
 		return "editCerealPage";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@PostMapping("/cereal/save")
-	//the cereal must be valid (tags in cereal.java)
-	//binding result checks if it is valid
+	// the cereal must be valid (tags in cereal.java)
+	// binding result checks if it is valid
 	public String saveCereal(@Valid Cereal cereal, BindingResult bindingResult, Model model) {
-		
-		if(bindingResult.hasErrors()) {
-			
+
+		if (bindingResult.hasErrors()) {
+
 			return "editCerealPage";
-			
-		}else {
-			
+
+		} else {
+
 			Cereal savedCereal = cerealRepository.save(cereal);
 			savedCereal.getCerealId();
-			//redirecting user to the page with the new cereal id which was assigned by the database
+			// redirecting user to the page with the new cereal id which was assigned by the
+			// database
 			return "redirect:/cereal/view/" + savedCereal.getCerealId();
-			
-			
+
 		}
-	
-		
+
 	}
 
-			
-		
-	
-	
 }
